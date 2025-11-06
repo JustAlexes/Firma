cdee2c86c20dacf184e18c651932bf6c6f7d63a8
JustAlexes <102738938+JustAlexes@users.noreply.github.com>
2025-11-06 17:58:20 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -3,49 +3,59 @@ import requests
 import pandas as pd
 import time
 
+from fake_useragent import UserAgent
 
 
+def seller_prices(seller_id: str | int, personal_discount: int):
+    print(f'Получение цен из каталога продавца на сайте\n')
 
-def parse_seller_page(seller_id: str | int, personal_discount: int):
     page = 1
+    appType = 1 # Веб версия
+    curr = 'rub' # Валюта
+    lang = 'ru' # Язык
+
 
     items = []
     quantity_products = 1
+    fua = UserAgent()
 
     while quantity_products != 0:
-        url = f'https://catalog.wb.ru/sellers/v4/catalog?ab_testing=false&ab_testing=false&appType=1&curr=rub&dest=123587633&hide_dtype=11&lang=ru&page={str(page)}&sort=popular&spp=30&supplier={str(seller_id)}&uclusters=1'
-        response = requests.get(url)
-        print(response.status_code)
-        data = response.json()
-        products = data.get('products', [])
-        quantity_products = (len(products))
-        print(quantity_products)
-        page = page + 1
-        time.sleep(5)
+        headers = {
+            'UserAgent': fua.random
+        }
+        url = f'https://catalog.wb.ru/sellers/v4/catalog?ab_testing=false&ab_testing=false&appType={str(appType)}&curr={curr}&dest=123587633&hide_dtype=11&lang={lang}&page={str(page)}&sort=popular&spp=10&supplier={str(seller_id)}&uclusters=1'
+        time.sleep(1)
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            products = data.get('products', [])
+            quantity_products = (len(products))
+            print(f'STATUS_CODE: {response.status_code}, quantity_products: {quantity_products}')
+            
+            if quantity_products != 0:
+                for product in products:
+                    price = product.get('sizes', [{}])[0].get('price', {}).get('product', 0) / 100
+                    personal_price = price * (1 - personal_discount/100)
+                    item = {
+                        'sku': product.get('id'),
+                        'name': product.get('name'),
+                        'price': math.floor(personal_price)
+                    }
+                    items.append(item)
+                page = page + 1
+                time.sleep(1)
 
-parse_seller_page(seller_id=53699, personal_discount=6)
+    print(f'Получено {len(items)} объектов')
+    try:
+        site_prices = pd.DataFrame(items)
+        site_prices.to_csv('site_prices.csv', index=False)
+        print(f'Прайс сохранен!')
+    except Exception as e:
+        print(f'Возникла ошибка сохранения: {e}')
 
-# while quantity_products != 0:
-#     response = requests.get(catalog_url)
-#     data = response.json()
-#     products = data.get('products', [])
-#     quantity_products = (len(products))
-    
-#     if quantity_products != 0:
-#         for product in products:
-#             price = product.get('sizes', [{}])[0].get('price', {}).get('product', 0) / 100
-#             personal_price = price * (1 - personal_discount/100)
+def main():
+    seller_prices(seller_id=53699, personal_discount=6)
 
-#             item = {
-#                 'sku': product.get('id'),
-#                 'name': product.get('name'),
-#                 'price': math.floor(personal_price)
-#             }
-
-#             items.append(item)
-
-#         page = page + 1
-# df = pd.DataFrame(items)
-# print(df.shape)
-
+if __name__ == '__main__':
+    main()
 
